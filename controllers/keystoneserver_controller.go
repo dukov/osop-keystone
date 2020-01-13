b4b797abaa6bac146c8b078dd2cc358cf133e2dc
Dmitry Ukov <dukov@mirantis.com>
2020-01-13 23:06:07 +0400

    Implement ConfigMap for Kyestone settings
    Change-Id: I9c1f51408d09aaaae6cf60eb965ebf966b99efce

--- controllers/keystoneserver_controller.go
+++ controllers/keystoneserver_controller.go
@@ -17,13 +17,23 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/go-logr/logr"
+	k8sapps "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	openstackv1alpha1 "github.com/dukov/osop-keystone/api/v1alpha1"
+)
+
+var (
+	ownerKey = ".metadata.controller"
+	apiGVStr = openstackv1alpha1.GroupVersion.String()
 )
 
 // KeystoneServerReconciler reconciles a KeystoneServer object
@@ -35,18 +45,83 @@ type KeystoneServerReconciler struct {
 
 // +kubebuilder:rbac:groups=openstack.osop.org,resources=keystoneservers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=openstack.osop.org,resources=keystoneservers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=openstack.osop.org,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=openstack.osop.org,resources=deployments/status,verbs=get
 
 func (r *KeystoneServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("keystoneserver", req.NamespacedName)
+	var keystoneSrv openstackv1alpha1.KeystoneServer
+	ctx := context.Background()
+	log := r.Log.WithValues("keystoneserver", req.NamespacedName)
+	if err := r.Get(ctx, req.NamespacedName, &keystoneSrv); err != nil {
+		log.Error(err, "unable to fetch Keystone servers")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// your logic here
+	var kDepls k8sapps.DeploymentList
+	if err := r.List(ctx, &kDepls, client.InNamespace(req.Namespace), client.MatchingField(ownerKey, req.Name)); err != nil {
+		log.Error(err, "deployments list error")
+		return ctrl.Result{}, err
+	}
+
+	cm, err := r.createConfigMap(keystoneSrv)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if err = r.Create(ctx, &cm); err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *KeystoneServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(&k8sapps.Deployment{}, ownerKey, func(rawObj runtime.Object) []string {
+		depl := rawObj.(*k8sapps.Deployment)
+		owner := metav1.GetControllerOf(depl)
+		if owner == nil {
+			return nil
+		}
+
+		if owner.APIVersion != apiGVStr || owner.Kind != "KeystoneServer" {
+			return nil
+		}
+
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&openstackv1alpha1.KeystoneServer{}).
+		Owns(&k8sapps.Deployment{}).
 		Complete(r)
+}
+
+func (r *KeystoneServerReconciler) createConfigMap(srv openstackv1alpha1.KeystoneServer) (corev1.ConfigMap, error) {
+	cfg := make(map[string]string)
+	var content []string
+	for section, opts := range srv.Spec.Config {
+		content = append(content, fmt.Sprintf("[%s]", section))
+		for key, val := range opts {
+			content = append(content, fmt.Sprintf("%s = %s", key, val))
+		}
+	}
+
+	cfg[KyestoneConfigFilename] = strings.Join(content, "\n")
+
+	cm := corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{APIVersion: corev1.SchemeGroupVersion.String(), Kind: "ConfigMap"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      srv.Name,
+			Namespace: srv.Namespace,
+		},
+		Data: cfg,
+	}
+
+	if err := ctrl.SetControllerReference(&srv, &cm, r.Scheme); err != nil {
+		return cm, err
+	}
+
+	return cm, nil
+
 }
